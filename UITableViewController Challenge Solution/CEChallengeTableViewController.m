0cc6a4870eaec5760c5178e1c4872477b524f8c6
Christopher R. Eady <ChrisMac@Christophers-MacBook-Pro.local>
2014-08-29 12:35:43 -0500

    added bg colors

--- UITableViewController Challenge Solution/CEChallengeTableViewController.m
+++ UITableViewController Challenge Solution/CEChallengeTableViewController.m
@@ -72,12 +72,15 @@
     // Configure the cell...
     if (indexPath.section == 0) {
         cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
     }
     else if (indexPath.section == 1) {
         cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
     }
     else {
         cell.textLabel.text = [NSString stringWithFormat:@"cell %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
     }
     return cell;
 }
